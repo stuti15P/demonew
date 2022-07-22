a280efd39c585ef78713204418a460aa7b4708cf
Stuti Priyambda <stuti.priyambda@BLRETV-KX96W3W7.local>
2022-07-22 10:04:40 +0530

    First Java project demo push

---
+++ src/com/flipkart/demo/DemoApp.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package com.flipkart.demo;
+
+/**
+ * @author stuti.priyambda
+ *
+ */
+public class DemoApp {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("This is my Demo Project");
+	}
+
+}
